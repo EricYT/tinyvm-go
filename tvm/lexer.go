02b2cf4ab9a50e24ce6c7658bb6ac309b3af664c
taoyu <eric.taoyu@gmail.com>
2018-01-05 14:09:39 +0800

    lexer code implement

--- tvm/lexer.go
+++ tvm/lexer.go
@@ -24,28 +24,35 @@ func (lexer *lexerCtx) Parse(src []byte, defines *htabCtx) {
 	// split all source into lines
 	lexer.sourceLines = bytes.Split(src, []byte("\n"))
 
-	lexer.tokens = make([][][]byte, len(lexer.sourceLines))
+	//lexer.tokens = make([][][]byte, len(lexer.sourceLines))
 
 	// split all lines into tokens
-	for lineIndex, line := range lexer.sourceLines {
+	for _, line := range lexer.sourceLines {
 		// filter comments delimited by '#'
-		if isComment(line) {
+		if isComment(line) || isSpaceLine(line) {
 			continue
 		}
 		// split lines
 		toks := bytes.FieldsFunc(line, func(c rune) bool {
-			return c == ' ' || c == '\t' || c == ','
+			// ' ' || '\t' || ','
+			return unicode.IsSpace(c) || c == ','
 		})
-		lexer.tokens[lineIndex] = make([][]byte, len(toks))
+		tokens := make([][]byte, len(toks))
+		//lexer.tokens[lineIndex] = make([][]byte, len(toks))
 		for tokIndex, tok := range toks {
 			if value, ok := defines.FindRef(string(tok)); ok {
-				lexer.tokens[lineIndex][tokIndex] = value
+				tokens[tokIndex] = value
 				continue
 			}
-			lexer.tokens[lineIndex][tokIndex] = tok
+			tokens[tokIndex] = tok
 		}
+		lexer.tokens = append(lexer.tokens, tokens)
 	}
 }
+
+func (lexer *lexerCtx) Lines() [][]byte { return lexer.sourceLines }
+
+func (lexer *lexerCtx) Tokens() [][][]byte { return lexer.tokens }
 
 func isComment(line []byte) bool {
 	clear := trimSpaceLeft(line)
@@ -59,4 +66,8 @@ func trimSpaceLeft(value []byte) []byte {
 	return bytes.TrimLeftFunc(value, func(c rune) bool {
 		return unicode.IsSpace(c)
 	})
+}
+
+func isSpaceLine(line []byte) bool {
+	return len(bytes.TrimSpace(line)) == 0
 }
--- tvm/lexer_test.go
+++ tvm/lexer_test.go
@@ -47,5 +47,80 @@ func TestTrimSpaceLeft(t *testing.T) {
 	if !bytes.Equal([]byte("foo"), trimSpaceLeft(val)) {
 		t.Fatalf("TestTrimSpaceLeft (%s) should be (foo) not (%s)", string(val), string(trimSpaceLeft(val)))
 	}
+}
 
+func TestIsSpaceLine(t *testing.T) {
+	val := []byte("xxx	\t dsfd\n")
+	if isSpaceLine(val) {
+		t.Fatalf("TestIsSpaceLine val(%s) is not space line.", string(val))
+	}
+	val = []byte(" \t	\n\n\t 	\t \n")
+	if !isSpaceLine(val) {
+		t.Fatalf("TestIsSpaceLine val(%v) is not space line.", val)
+	}
+}
+
+var src string = `
+	# I'm a comment.
+start:
+  mov eax, 42
+	mov ebx, ONE
+	call print_eax
+
+	mov		eax,					23
+	mov ebx,	ZERO
+	call		  print_eax
+
+`
+
+var tokens [][][]byte = [][][]byte{
+	[][]byte{
+		[]byte("start:"),
+	},
+	[][]byte{
+		[]byte("mov"),
+		[]byte("eax"),
+		[]byte("42"),
+	},
+	[][]byte{
+		[]byte("mov"),
+		[]byte("ebx"),
+		[]byte("1"),
+	},
+	[][]byte{
+		[]byte("call"),
+		[]byte("print_eax"),
+	},
+	[][]byte{
+		[]byte("mov"),
+		[]byte("eax"),
+		[]byte("23"),
+	},
+	[][]byte{
+		[]byte("mov"),
+		[]byte("ebx"),
+		[]byte("0"),
+	},
+	[][]byte{
+		[]byte("call"),
+		[]byte("print_eax"),
+	},
+}
+
+func TestLexerParse(t *testing.T) {
+	def := NewHtabCtx()
+	def.AddRef("ONE", []byte("1"))
+	def.AddRef("ZERO", []byte("0"))
+
+	lexer := NewLexerCtx()
+	lexer.Parse([]byte(src), def)
+
+	for lineIndex, lineTokens := range lexer.Tokens() {
+		for tokenIndex, token := range lineTokens {
+			rightToken := tokens[lineIndex][tokenIndex]
+			if !bytes.Equal(token, rightToken) {
+				t.Fatalf("TestLexerParse lexer parse line(%d) token(%d) value(%#v) got wrong tokens(%#v).", lineIndex, tokenIndex, rightToken, token)
+			}
+		}
+	}
 }
